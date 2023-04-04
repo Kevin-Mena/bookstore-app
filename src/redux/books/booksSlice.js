1d7b3dc0d13db248bb75124eaef6d8259e9b0ead
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-04 13:04:26 +0300

    Add feature folder

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      const { author, title } = action.payload;
+      const uniqueID = uuid();
+      const id = uniqueID.slice(0, 4);
+      const newBook = { id, author, title };
+      return { ...state, books: [...state.books, newBook] };
+    },
+
+    removeBook: (state, action) => {
+      const bookId = action.payload;
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== bookId),
+      };
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
