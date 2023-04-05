357a729ed440ad4b2c66e1c20e8b1f27880197fb
Kevin Okoth <kevinokotth04@gmail.com>
2023-04-05 20:21:22 +0300

    Modify extraReducers

--- src/features/books/booksSlice.js
+++ src/features/books/booksSlice.js
@@ -1,11 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { v4 as uuid } from "uuid";
+import axios from "axios";
 
 const initialState = {
   bookItems: [],
-  isLoading: null,
-  error: null,
+  isLoading: true,
+  bookAdded: true,
+  bookDeleted: true,
 };
+
+const BOOK_ID = "IDVWqORBUvdJWqz3wMbN";
+const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${BOOK_ID}/books`;
+
+export const fetchBooks = createAsyncThunk("book/fetchBooks", async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
+
+export const createBook = createAsyncThunk("books/createBook", async (book) => {
+  try {
+    const response = await axios.post(API_URL, book);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
+
+export const deleteBook = createAsyncThunk(
+  "book/deleteBook",
+  async (bookId) => {
+    try {
+      const response = await axios.delete(`${API_URL / bookId}`);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
 
 const booksSlice = createSlice({
   name: "book",
@@ -30,6 +65,47 @@ const booksSlice = createSlice({
         bookItems: state.bookItems.filter((book) => book.id !== bookId),
       };
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(fetchBooks.fulfilled, (state,action) => {
+        state.isLoading = true;
+        state.bookItems= action.payload;
+      })
+      .addCase(fetchBooks.rejected, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(createBook.pending, (state) => {
+        state.isLoading = true;
+        state.bookAdded = false;
+      })
+      .addCase(createBook.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.bookItems.push(action.payload);
+        state.bookAdded = true;
+      })
+      .addCase(createBook.rejected, (state) => {
+        state.isLoading = false;
+        state.bookAdded = false;
+      });
+      .addCase(bookDeleted.pending, (state) => {
+        state.isLoading = true;
+        state.bookAdded = false;
+      });
+      .addCase(bookDeleted.pending, (state,action) => {
+        state.isLoading = false;
+        state.bookItems = state.bookItems.filter(
+        (book) => book.id !== action.payload
+      );
+        state.bookAdded = true;
+      });
+      .addCase(bookDeleted.rejected, (state) => {
+        state.isLoading = false;
+        state.bookAdded = false;
+      });
   },
 });
 
